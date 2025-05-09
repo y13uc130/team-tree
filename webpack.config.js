9fd50db0815343af5e4db3629ce64d42353ed629
intel.kancharla <intel.kancharla@gameskraft.com>
2025-05-09 20:12:33 +0530

    changes to deploy on netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,91 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: isProduction ? "bundle.[contenthash].js" : "[name].bundle.js",
     publicPath: "/",
     clean: true,
   },
-  mode: "development",
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? false : "source-map",
+
   devServer: {
-    static: [
-      {
-        directory: path.resolve(__dirname, "dist"),
-      },
-      {
-        directory: path.resolve(__dirname, "public"),
-        publicPath: "/",
-      },
-    ],
-    hot: true,
+    static: {
+      directory: path.resolve(__dirname, "public"),
+      publicPath: "/",
+    },
     historyApiFallback: true,
+    hot: true,
+    port: 3000,
   },
+
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "postcss-loader",
+        ],
       },
       {
-        test: /\.scss$/, // Match SCSS files
-        use: ["style-loader", "css-loader", "sass-loader"], // Loaders to process SCSS
+        test: /\.scss$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
+        type: "asset",
       },
     ],
   },
+
   resolve: {
     extensions: [".js", ".jsx"],
   },
+
+  optimization: {
+    minimize: isProduction,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+      minify: isProduction
+        ? {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+          }
+        : false,
     }),
+    ...(isProduction
+      ? [
+          new MiniCssExtractPlugin({
+            filename: "styles.[contenthash].css",
+          }),
+        ]
+      : []),
   ],
 };
